308404a64688a6aa57ef61adf85df581b8aa323a
garysenoc <gsenoc@gmail.com>
2022-02-02 14:16:50 +0800

    Change local mongoose to online

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
 mongoose
-  .connect("mongodb://localhost:27017/yasalam")
+  .connect(
+    "mongodb+srv://dbYasalam:Xp9iuzszFA56iSJG@cluster0.r2c1j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  )
   .then(() => console.log("DB connection established"));
 
 const port = process.env.PORT || 8000;
